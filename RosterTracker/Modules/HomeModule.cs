2cde5ae2507be48ec5a5c900f80ea4c62f975013
mwinger <mateo6379@gmail.com>
2015-11-27 14:58:13 -0600

    Initial working Nancy module.

---
+++ RosterTracker/Modules/HomeModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RosterTracker
+{
+	public class HomeModule : Nancy.NancyModule
+	{
+		public HomeModule ()
+			:base ("/")
+		{
+			Get [""] = parameters => "Welcome home!";
+		}
+	}
+}
+
